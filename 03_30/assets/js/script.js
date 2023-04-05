c086399d537a680fbb97c8599b31a87cac26615d
minkyum03 <kimminkyum03@gmail.com>
2023-04-05 16:30:09 +0900

    03_30
    Slick Slider1

---
+++ 03_30/assets/js/script.js
@@ -0,0 +1,11 @@
+$('.myslider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    dots: true,
+});
